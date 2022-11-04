2e950eff9c8d3550e204f9063d44721720dd7839
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-11-04 16:15:56 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { PostsTable } from './components/PostsTable';
+import { CreatePost } from './components/CreatePost';
+
+function App() {
+  return (
+    <div className="App">
+      <PostsTable />
+      <CreatePost />
+    </div>
+  );
+}
+
+export default App;
